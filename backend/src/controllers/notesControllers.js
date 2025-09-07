94a423a5da55402bc23b58324942d3dda829400a
cookie-kun <piyushcalling09@gmail.com>
2025-09-07 22:09:25 +0530

    added basic routes and middlewares

--- backend/src/controllers/notesControllers.js
+++ backend/src/controllers/notesControllers.js
@@ -2,23 +2,51 @@ import Note from "../models/Note.js";
 
 export const getAllNotes = async (req, res) => {
 	try {
-		const notes = await Note.find();
+		const notes = await Note.find().sort({ createdAt: 1});
 		res.status(200).json({ success: true, data: notes });
 	} catch (error) {
 		res.status(500).json({ success: false, message: "Failed to fetch notes", error: error.message });
 	}
+}; 
+
+export const getNoteById = async (req, res) => {
+	try {
+		const note = await Note.findById(req.params.id);
+		if(!note) return res.status(404).json({message: "user does not exist!"});
+		res.status(200).json(note);
+	} catch (error){
+		res.status(500).json()({ success: false, message: "Error in getNoteById controller", error: error.message})
+	}
 };
 
-export const createNote = (req, res) => {
-	res.status(201).json({message: "Note created successfully"})
+export const createNote = async (req, res) => {
+	try {
+		const { title, content } = req.body
+		const newNote = new Note({ title, content })
+		const savedNote = await newNote.save();
+		res.status(201).send(savedNote)
+	} catch (error) {
+		res.status(500).json({ success: false, message: "Failed to create note, error in createNote controller", error: error.message });
+	}
 }
 
-export const updateNote = (req, res) => {
-	const { id } = req.params;
-	res.status(200).json({message: `Note with id: ${id} updated successfully`})
+export const updateNote = async (req, res) => {
+	try {
+		const { title, content} = req.body
+		const updatedNote = await Note.findByIdAndUpdate(req.params.id, { title, content }, {new: true})
+		if(!updateNote) return res.status(404).json({message: "user not found"})
+		res.status(200).json(updatedNote)
+	} catch (error){	
+		res.status(500).json({ success: false, message: "Failed to create note, error in updateNote controller", error: error.message });
+	}
 }
 
-export const deleteNote = (req, res) => {
-	const { id } = req.params;
-	res.status(200).json({message: `Note with id: ${id} deleted successfully`})
+export const deleteNote = async (req, res) => {	
+	try {
+		const note = await Note.findByIdAndDelete(req.params.id)
+		if(!note) return res.status(404).json({message: "user not found"})
+		res.status(200).json({message: `user deleted successfully`})
+	} catch (error){
+			res.status(500).json({ success: false, message: "Failed to delete note, error in updateNote controller", error: error.message });
+	}
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import notesRoutes from "./routes/notesRoutes.js";	
 import connectDB from "./config/db.js"
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
-connectDB();
+app.use(express.json()); 
+app.use(rateLimiter)
+app.use(express.urlencoded({ extended: true })); 
 	
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {	
-  console.log(`Server running on port ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {	
+    console.log(`Server running on port ${PORT}`);
+  });
 });

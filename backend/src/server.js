021f0b66d1807c9d302de81c79a08f96dc58d51b
cookie-kun <piyushcalling09@gmail.com>
2025-09-09 00:21:15 +0530

    added note-details page and finalised

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT;
 app.use(express.json()); 
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: "http://localhost:5173" 
 })) 
 app.use(rateLimiter)
 
